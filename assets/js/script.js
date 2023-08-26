ab75b5286b729e3f02ea7a090d89c68cb41bc830
David O'Gara <davidogara@hotmail.co.uk>
2023-08-26 16:59:03 +0100

    Add variables in script for game-screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-// JavaScript Document
+// Welcome to the JavaScript Document
+
+// Setting variables
+const titleCard = document.getElementById('title-card');
+const creatureBox = document.getElementById('creature-box');
+const questionBox = document.getElementById('question-box');
+
+// Setting username variable when entered...
+const userName = document.getElementById('name');
+
