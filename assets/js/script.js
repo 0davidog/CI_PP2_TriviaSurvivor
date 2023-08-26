5c8331981e2a3f46e03e21074263831a81a9237a
David O'Gara <davidogara@hotmail.co.uk>
2023-08-26 17:00:20 +0100

    Add function to log username value to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,9 @@ const questionBox = document.getElementById('question-box');
 // Setting username variable when entered...
 const userName = document.getElementById('name');
 
+//logging username to console...
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', logName);
+function logName() {
+	console.log(`Username: ${userName.value}`);
+}
