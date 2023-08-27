935551ffe6f1ac3145969aeb8597bf550501cf03
David O'Gara <davidogara@hotmail.co.uk>
2023-08-27 10:17:42 +0100

    Add functions that cycle through intro messages after mode select

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ const titleCard = document.getElementById('title-card');
 const creatureBox = document.getElementById('creature-box');
 const questionBox = document.getElementById('question-box');
 const textBox = document.getElementById('text-box');
-const nextBtn = document.getElementById('next-btn');
 
 // Setting username variable when entered...
 const userName = document.getElementById('name');
@@ -15,6 +14,7 @@ const startBtn = document.getElementById('start-btn');
 startBtn.addEventListener('click', logName);
 function logName() {
 	console.log(`Username: ${userName.value}`);
+	textBox.innerHTML = `<p>Welcome ${userName.value}.</p>`;
 }
 
 // Taking user to difficulty selection...
@@ -29,11 +29,12 @@ function chooseDifficulty() {
 const easyBtn = document.getElementById('easy-btn');
 const normalBtn = document.getElementById('normal-btn');
 const hardBtn = document.getElementById('hard-btn');
-easyBtn.addEventListener('click', startQuiz);
-normalBtn.addEventListener('click', startQuiz);
-hardBtn.addEventListener('click', startQuiz);
+easyBtn.addEventListener('click', modeSelected);
+normalBtn.addEventListener('click', modeSelected);
+hardBtn.addEventListener('click', modeSelected);
 
-function startQuiz(event) {
+// Setting the options according to mode chosen...
+function modeSelected(event) {
 	let mode = event.target.value;
 	if (mode === "easy") {
 		console.log('Easy mode started...');
@@ -47,5 +48,36 @@ function startQuiz(event) {
 	titleCard.style.display = "none";
 	creatureBox.classList.remove('hidden');
 	questionBox.classList.remove('hidden');
-	textBox.innerHTML = `<p>Welcome ${userName.value}</p>`;
+	introMessage01();
+}
+
+// Adding functions for cycling through the intro messages...
+function introMessage01() {
+	let nexBtn01 = document.getElementById('next-btn-01');
+	nexBtn01.classList.remove('hidden');
+    nexBtn01.addEventListener('click', introMessage02);
+}
+function introMessage02() {
+	textBox.innerHTML = `<p>You have entered a world of survival trivia.</p>`;
+	let nexBtn01 = document.getElementById('next-btn-01');
+	nexBtn01.classList.add('hidden');
+	let nexBtn02 = document.getElementById('next-btn-02');
+	nexBtn02.classList.remove('hidden');
+    nexBtn02.addEventListener('click', introMessage03);
+}
+function introMessage03() {
+	textBox.innerHTML = `<p>Answer questions correctly to stall the creature.</p>`;
+	let nexBtn02 = document.getElementById('next-btn-02');
+	nexBtn02.classList.add('hidden');
+	let nexBtn03 = document.getElementById('next-btn-03');
+	nexBtn03.classList.remove('hidden');
+    nexBtn03.addEventListener('click', introMessage04);
+}
+function introMessage04() {
+	textBox.innerHTML = `<p>Good luck.</p>`;
+	let nexBtn03 = document.getElementById('next-btn-03');
+	nexBtn03.classList.add('hidden');
+	let nexBtn04 = document.getElementById('next-btn-04');
+	nexBtn04.classList.remove('hidden');
+    nexBtn04.addEventListener('click', startQuiz);
 }
