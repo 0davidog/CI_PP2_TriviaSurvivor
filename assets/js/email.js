aa48beb1d03b425d000c3851d8558d7aac769791
David O'Gara <davidogara@hotmail.co.uk>
2023-09-13 10:30:23 +0100

    Fix typo in emailjs script

--- assets/js/email.js
+++ assets/js/email.js
@@ -16,22 +16,23 @@ window.onload = function () {
 		.getElementById("contact-form")
 		.addEventListener("submit", function (event) {
 		event.preventDefault();
+		document.getElementById('contact-form').disabled = true;
 		// generate a five digit number for the contact_number variable
 		this.contact_number.value = (Math.random() * 100000) | 0;
             // these IDs from the previous steps
 		emailjs.sendForm("service_pmzmnla", "contact-form", this).then(
 			function () {
 				document.getElementById("contact-form").reset();
-				document.getElementById('contact-form').disabled = true;
 				modal_message.innerHTML = 'Thank you for your feedback.';
 				modal.style.display = "flex";
-				document.getElementById('feedback-text">').innerHTML="Thank you, your feedback has been recieved and will be taken into consideration.";
+				document.getElementById('feedback-text').innerHTML="Thank you, your feedback has been recieved and will be taken into consideration.";
 				document.getElementById('contact-form').display="none";
 			},
 			function (error) {
 				modal_message.innerHTML = 'Sorry, message failed to send.';
 				modal.style.display = "flex";
-				document.getElementById('feedback-text">').innerHTML="Sorry, as your feedback failed to send please consider trying again.";
+				document.getElementById('feedback-text').innerHTML="Sorry, as your feedback failed to send please consider trying again.";
+				document.getElementById('contact-form').disabled = false;
 			}
 		);
 	});
